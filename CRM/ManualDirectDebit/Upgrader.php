56477cf3fb7ea9216835d560b8a1b25a25db73b5
Bohdan Dmytryshyn <bohdan.dmytryshyn@agiliway.com>
2018-02-21 15:35:41 +0200

    DD-5: Direct Debit Mandate Custom Fields

---
+++ CRM/ManualDirectDebit/Upgrader.php
@@ -0,0 +1,40 @@
+<?php
+use CRM_ManualDirectDebit_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_ManualDirectDebit_Upgrader extends CRM_ManualDirectDebit_Upgrader_Base {
+
+  // By convention, functions that look like "function upgrade_NNNN()" are
+  // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
+
+  /**
+   * Example: Run an external SQL script when the module is installed.
+   */
+  public function install() {
+    $this->executeSqlFile('sql/install.sql');
+  }
+
+  /**
+   * Example: Run a simple query when a module is enabled.
+   */
+  public function enable() {
+
+  }
+
+  /**
+   * Example: Run a simple query when a module is disabled.
+   */
+  public function disable() {
+
+  }
+
+  /**
+   * Example: Run an external SQL script when the module is uninstalled.
+   */
+  public function uninstall() {
+    $this->executeSqlFile('sql/uninstall.sql');
+  }
+
+}
---
+++ sql/uninstall.sql
@@ -0,0 +1,7 @@
+-- /*******************************************************
+-- * Delete direct debit tables
+-- *******************************************************/
+
+--SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `dd_contribution_recurr_mandate_ref`;
