d82f869cc794421e076d1d1cda80e31f4e76abc9
Bohdan Dmytryshyn <bohdan.dmytryshyn@agiliway.com>
2018-02-22 13:21:36 +0200

    DD-5: Remove unused code

--- sql/uninstall.sql
+++
@@ -1,7 +0,0 @@
--- /*******************************************************
--- * Delete direct debit tables
--- *******************************************************/
-
---SET FOREIGN_KEY_CHECKS=0;
-
-DROP TABLE IF EXISTS `dd_contribution_recurr_mandate_ref`;
