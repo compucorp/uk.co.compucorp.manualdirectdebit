b18e0df3c8842057d77b2f93ee1a012f8ae70403
Bohdan Dmytryshyn <bohdan.dmytryshyn@agiliway.com>
2018-02-28 17:25:02 +0200

    DD-6: Add 'dd_contribution_recurr_mandate_ref' custom schema. Add read-only mandate_id custom field.

--- CRM/ManualDirectDebit/Upgrader.php
+++ CRM/ManualDirectDebit/Upgrader.php
@@ -5,4 +5,13 @@ use CRM_ManualDirectDebit_ExtensionUtil as E;
  * Collection of upgrade steps.
  */
 class CRM_ManualDirectDebit_Upgrader extends CRM_ManualDirectDebit_Upgrader_Base {
+
+  public function install() {
+    $this->executeSqlFile('sql/install.sql');
+  }
+
+  public function uninstall() {
+    $this->executeSqlFile('sql/uninstall.sql');
+  }
+
 }
