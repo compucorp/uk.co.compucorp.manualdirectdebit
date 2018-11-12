08c2c5b6c34491fc947ca62c1275dbc4776f01b1
Camilo Rodriguez <camilo@compucorp.co.uk>
2018-11-12 19:55:05 +0000

    DD-64: Fix Generation of Mandate Reference

--- CRM/ManualDirectDebit/Hook/Custom/Mandate/MandateDataGenerator.php
+++ CRM/ManualDirectDebit/Hook/Custom/Mandate/MandateDataGenerator.php
@@ -67,7 +67,7 @@ class CRM_ManualDirectDebit_Hook_Custom_Mandate_MandateDataGenerator {
       }
     }
 
-    if ($this->fieldsToGenerate['dd_ref'] == FALSE) {
+    if ($this->fieldsToGenerate['dd_ref'] == FALSE || $this->fieldsToGenerate['dd_ref'] == 'DD Ref') {
       $this->fieldsToGenerate['dd_ref'] = $this->generateDirectDebitReference();
     }
 
