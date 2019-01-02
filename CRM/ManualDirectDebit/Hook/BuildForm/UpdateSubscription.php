1aaff264c7bdf834f424ee2376467a938c73d30c
Camilo Rodriguez <camilo@compucorp.co.uk>
2019-01-02 17:04:38 +0000

    DD-64: Separate Setting of ContactID vs Mandate ID into Different Methods

--- CRM/ManualDirectDebit/Hook/BuildForm/UpdateSubscription.php
+++ CRM/ManualDirectDebit/Hook/BuildForm/UpdateSubscription.php
@@ -25,9 +25,32 @@ class CRM_ManualDirectDebit_Hook_BuildForm_UpdateSubscription {
    * Implements buildForm hook.
    */
   public function buildForm() {
+    $this->addContactIDToCoreFormJSVariable();
+    $this->addMandateIDToCoreFormJSVariable();
+  }
+
+  /**
+   * On UpdateSubscription form, payment details are loaded in by an ajax call
+   * and sends the contact_id, if it is defined in CRM.coreForm.contact_id. If
+   * it's not defined, contact_id is determined by CiviCRM, defaulting to the
+   * current user. This contact ID is needed to obtain the list of mandates the
+   * contact has, and thus allow them to be selected in the form.
+   *
+   * This method sets the contact ID on the CRM.coreForm global variable, used
+   * by CiviCRM to make the call that loads the fields associated to the payment
+   * method.
+   */
+  private function addContactIDToCoreFormJSVariable() {
     $contactID = CRM_Utils_Request::retrieve('cid', 'Positive', $this->form);
     CRM_Core_Resources::singleton()->addVars('coreForm', array('contact_id' => (int) $contactID));
+  }
 
+  /**
+   * Sets the mandate ID asociated to the recurring contribution as a global JS
+   * variable added to CRM.coreForm.selected_mandate_id, so it can be used to
+   * select the appropriate option on the selection box combo.
+   */
+  private function addMandateIDToCoreFormJSVariable() {
     $recurringContributionID = CRM_Utils_Request::retrieve('crid', 'Positive', $this->form);
     $selectedMandateID = CRM_ManualDirectDebit_BAO_RecurrMandateRef::getMandateIdForRecurringContribution($recurringContributionID);
 
