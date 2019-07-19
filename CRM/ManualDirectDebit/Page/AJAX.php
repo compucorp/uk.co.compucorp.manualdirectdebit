2daf8de19a9b36d937e3211ece985c80b90770c2
Nishant <nishant.bhorodia@gmail.com>
2019-07-19 18:48:43 +0530

    DD-72: Only add receive_date for dd_payments batch types

--- CRM/ManualDirectDebit/Page/AJAX.php
+++ CRM/ManualDirectDebit/Page/AJAX.php
@@ -20,7 +20,6 @@ class CRM_ManualDirectDebit_Page_AJAX {
       5 => 'amount',
       6 => 'reference_number',
       7 => 'transaction_type',
-      8 => 'receive_date',
     ];
 
     $sEcho = CRM_Utils_Request::retrieveValue('sEcho', 'Integer');
@@ -31,6 +30,11 @@ class CRM_ManualDirectDebit_Page_AJAX {
     $entityID = CRM_Utils_Request::retrieveValue('entityID', 'String', NULL);
     $entityTable = CRM_Utils_Request::retrieveValue('entityTable', 'String', NULL);
     $notPresent = CRM_Utils_Request::retrieveValue('notPresent', 'String', NULL);
+
+    $batch = (new CRM_ManualDirectDebit_Batch_BatchHandler($entityID));
+    if($batch->getBatchType() == 'dd_payments') {
+      $sortMapper[] = 'receive_date';
+    }
 
     $sort = CRM_Utils_Array::value(CRM_Utils_Request::retrieveValue('iSortCol_0', 'Integer', NULL), $sortMapper);
     $sortOrder = CRM_Utils_Request::retrieveValue('sSortDir_0', 'String', 'asc');
@@ -67,9 +71,12 @@ class CRM_ManualDirectDebit_Page_AJAX {
       'amount',
       'reference_number',
       'transaction_type',
-      'receive_date',
-      'action',
     ];
+
+    if($batch->getBatchType() == 'dd_payments') {
+      $selectorElements[] = 'receive_date';
+    }
+    $selectorElements[] = 'action';
 
     if ($return) {
       return CRM_Utils_JSON::encodeDataTableSelector($mandateItems, $sEcho, $iTotal, $iFilteredTotal, $selectorElements);
