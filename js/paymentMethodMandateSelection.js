d981910f2463ce0916e81efb4bcf410722e16ac7
Camilo Rodriguez <camilo@compucorp.co.uk>
2019-12-30 16:40:59 +0000

    CGP-1472: Implement Mandate Refresh After Failed Validation

--- js/paymentMethodMandateSelection.js
+++ js/paymentMethodMandateSelection.js
@@ -2,16 +2,31 @@ CRM.$(function ($) {
   let contactField = $("#contact_id");
   let paymentInstrumentField = $("#payment_instrument_id");
 
-  if (contactField.val() !== "" && contactField.val() != null  && typeof CRM.vars.coreForm !== "undefined") {
+  if (typeof contactField.val() !== "undefined" && contactField.val() !== "" && typeof CRM.vars.coreForm !== "undefined") {
     CRM.vars.coreForm.contact_id = contactField.val();
   }
-
-  contactField.change(function () {
-    CRM.vars.coreForm.contact_id = $(this).val();
-    CRM.$("#payment_instrument_id").trigger("change.paymentBlock");
-  });
 
   if (paymentInstrumentField.val() !== "") {
     paymentInstrumentField.trigger("change.paymentBlock");
   }
+
+  contactField.change(function () {
+    CRM.vars.coreForm.contact_id = $(this).val();
+    $("#payment_instrument_id").trigger("change.paymentBlock");
+  });
+
+  paymentInstrumentField.closest('.ui-dialog').on('crmFormLoad.crmForm', function () {
+    $("#payment_instrument_id").trigger("change.paymentBlock");
+  });
+
+  paymentInstrumentField.closest('.ui-dialog').on('crmFormError.crmForm', function () {
+    $("#billing-payment-block").on('crmLoad', function() {
+      let mandateField = $("#mandate_id");
+      let mandateFieldLabel = $("label[for='mandate_id']");
+      if (typeof mandateField.val() !== "undefined" && mandateField.val() === "-") {
+        mandateField.addClass("error crm-error");
+        mandateFieldLabel.addClass("error crm-error");
+      }
+    });
+  });
 });
