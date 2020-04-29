f5969d9359682e01ece8bb4aeb3d2166dd99bbac
Omar abu hussein <opetmar91@gmail.com>
2020-04-29 21:30:31 +0300

    MAE-290: Refactoring BatchSubmissionQueue page

--- CRM/ManualDirectDebit/Page/BatchSubmissionQueue.php
+++ CRM/ManualDirectDebit/Page/BatchSubmissionQueue.php
@@ -1,21 +1,19 @@
 <?php
 
-class CRM_ManualDirectDebit_Page_BatchSubmissionQueue extends CRM_Core_Page {
+use CRM_ManualDirectDebit_Queue_Build_InstructionsQueueBuilder as InstructionsQueueBuilder;
+use CRM_ManualDirectDebit_Queue_Build_PaymentsQueueBuilder as PaymentsQueueBuilder;
 
-  private $batchRecordsLimit;
+class CRM_ManualDirectDebit_Page_BatchSubmissionQueue extends CRM_Core_Page {
 
   private $queue;
 
   private $batchId;
-
-  private $taskItemRecords = [];
 
   public function __construct($title = NULL, $mode = NULL) {
     parent::__construct($title, $mode);
 
     $this->queue = CRM_ManualDirectDebit_Queue_BatchSubmission::getQueue();
     $this->batchId =  CRM_Utils_Request::retrieveValue('batchId', 'Int');
-    $this->batchRecordsLimit = CRM_ManualDirectDebit_Common_SettingsManager::getBatchSubmissionRecordsPerTaskLimit();
   }
 
   public function run() {
@@ -46,12 +44,13 @@ class CRM_ManualDirectDebit_Page_BatchSubmissionQueue extends CRM_Core_Page {
     $batchType = $this->getBatchType();
     switch ($batchType) {
       case 'instructions_batch':
-        $this->addInstructionsQueueTasks();
+        $queueBuilder = new InstructionsQueueBuilder($this->queue, $this->batchId);
         break;
       case 'dd_payments':
-        $this->addPaymentsQueueTasks();
+        $queueBuilder = new PaymentsQueueBuilder($this->queue, $this->batchId);
         break;
     }
+    $queueBuilder->buildQueue();
 
     $this->addBatchSubmissionCompletionTask();
   }
@@ -59,86 +58,8 @@ class CRM_ManualDirectDebit_Page_BatchSubmissionQueue extends CRM_Core_Page {
   private function getBatchType() {
     $batch = CRM_Batch_DAO_Batch::findById($this->batchId);
     $batchTypes = CRM_Core_PseudoConstant::get('CRM_Batch_DAO_Batch', 'type_id', ['labelColumn' => 'name']);
+
     return $batchTypes[$batch->type_id];
-  }
-
-  private function addInstructionsQueueTasks() {
-    $rows = civicrm_api3('EntityBatch', 'get', [
-      'return' => ['entity_id'],
-      'options' => ['limit' => 0],
-      'sequential' => 1,
-      'batch_id' => $this->batchId,
-    ]);
-
-    if (empty($rows['values'])) {
-      return;
-    }
-    $rows = $rows['values'];
-
-    foreach ($rows as $row) {
-      if (count($this->taskItemRecords) >= $this->batchRecordsLimit) {
-        $this->addInstructionsQueueTaskItem();
-        $this->taskItemRecords = [];
-      }
-
-      $mandateId = CRM_Utils_Array::value('entity_id', $row);
-      $this->taskItemRecords[] = ['mandate_id' => $mandateId];
-    }
-
-    if (!empty($this->taskItemRecords)) {
-      $this->addInstructionsQueueTaskItem();
-    }
-  }
-
-  private function addInstructionsQueueTaskItem() {
-    $mandateIds = implode(', ', array_column($this->taskItemRecords, 'mandate_id'));
-    $taskTitle = 'Processing the mandates with these Ids : ' . $mandateIds;
-
-    $task = new CRM_Queue_Task(
-      ['CRM_ManualDirectDebit_Queue_Task_BatchSubmission_InstructionItem', 'run'],
-      [$this->taskItemRecords],
-      $taskTitle
-    );
-    $this->queue->createItem($task);
-  }
-
-  private function addPaymentsQueueTasks() {
-    $sqlQuery = 'SELECT mandate.entity_id as contribution_id, mandate.mandate_id as mandate_id 
-               FROM s1mev2civi_mstkv.civicrm_entity_batch entity_batch 
-               LEFT JOIN civicrm_value_dd_information mandate ON entity_batch.entity_id = mandate.entity_id  
-               WHERE entity_batch.batch_id = %1;';
-    $result = CRM_Core_DAO::executeQuery($sqlQuery, [
-      1 => [$this->batchId, 'Integer'],
-    ]);
-
-    while ($result->fetch()) {
-      if (count($this->taskItemRecords) >= $this->batchRecordsLimit) {
-        $this->addPaymentQueueTaskItem();
-        $this->taskItemRecords = [];
-      }
-
-      $row = $result->toArray();
-      $mandateId = CRM_Utils_Array::value('mandate_id', $row);
-      $contributionId = CRM_Utils_Array::value('contribution_id', $row);
-      $this->taskItemRecords[] = ['mandate_id' => $mandateId, 'contribution_id' => $contributionId];
-    }
-
-    if (!empty($this->taskItemRecords)) {
-      $this->addPaymentQueueTaskItem();
-    }
-  }
-
-
-  private function addPaymentQueueTaskItem() {
-    $contributionIds = implode(', ', array_column($this->taskItemRecords, 'contribution_id'));
-    $taskTitle = 'Processing the contributions with these Ids : ' . $contributionIds;
-
-    $task = new CRM_Queue_Task(
-      ['CRM_ManualDirectDebit_Queue_Task_BatchSubmission_PaymentItem', 'run'],
-      [$this->taskItemRecords],
-      $taskTitle
-    );
-    $this->queue->createItem($task);
   }
 
   private function addBatchSubmissionCompletionTask() {
---
+++ CRM/ManualDirectDebit/Queue/Build/PaymentsQueueBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+class CRM_ManualDirectDebit_Queue_Build_PaymentsQueueBuilder extends CRM_ManualDirectDebit_Queue_Build_BaseBuilder {
+
+  public function buildQueue() {
+    $sqlQuery = 'SELECT mandate.entity_id as contribution_id, mandate.mandate_id as mandate_id 
+               FROM s1mev2civi_mstkv.civicrm_entity_batch entity_batch 
+               LEFT JOIN civicrm_value_dd_information mandate ON entity_batch.entity_id = mandate.entity_id  
+               WHERE entity_batch.batch_id = %1;';
+    $result = CRM_Core_DAO::executeQuery($sqlQuery, [
+      1 => [$this->batchId, 'Integer'],
+    ]);
+
+    while ($result->fetch()) {
+      if (count($this->taskItemRecords) >= $this->batchRecordsLimit) {
+        $this->addPaymentQueueTaskItem();
+        $this->taskItemRecords = [];
+      }
+
+      $row = $result->toArray();
+      $mandateId = CRM_Utils_Array::value('mandate_id', $row);
+      $contributionId = CRM_Utils_Array::value('contribution_id', $row);
+      $this->taskItemRecords[] = ['mandate_id' => $mandateId, 'contribution_id' => $contributionId];
+    }
+
+    if (!empty($this->taskItemRecords)) {
+      $this->addPaymentQueueTaskItem();
+    }
+  }
+
+  private function addPaymentQueueTaskItem() {
+    $contributionIds = implode(', ', array_column($this->taskItemRecords, 'contribution_id'));
+    $taskTitle = 'Processing the contributions with these Ids : ' . $contributionIds;
+
+    $task = new CRM_Queue_Task(
+      ['CRM_ManualDirectDebit_Queue_Task_BatchSubmission_PaymentItem', 'run'],
+      [$this->taskItemRecords],
+      $taskTitle
+    );
+    $this->queue->createItem($task);
+  }
+}
