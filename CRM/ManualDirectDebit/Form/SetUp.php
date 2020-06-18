44f6e55f5c28f94c4a947c3bb0c6d8edd10b14af
Erawat Chamanont <erawat@gmail.com>
2020-06-18 15:38:44 +0100

    RSE-1147: Add functiionality after saving the form and fix php warning

--- CRM/ManualDirectDebit/Form/SetUp.php
+++ CRM/ManualDirectDebit/Form/SetUp.php
@@ -19,7 +19,7 @@ class CRM_ManualDirectDebit_Form_SetUp extends CRM_Core_Form {
     if ($this->contributionId == NULL) {
       CRM_Utils_System::redirect('/');
     }
-    CRM_Utils_System::setTitle(E::ts('Set up a Direct Debit'));
+    CRM_Utils_System::setTitle(E::ts('Set up your Direct Debit'));
   }
 
   public function buildQuickForm() {
@@ -49,7 +49,7 @@ class CRM_ManualDirectDebit_Form_SetUp extends CRM_Core_Form {
     if (count($paymentDates) == 1) {
       $this->assign('payment_date_value', reset($paymentDates));
       $this->add('hidden', 'payment_dates', key($paymentDates));
-    }else {
+    } else {
       $this->add('select', 'payment_dates', E::ts('Payment Date'), $paymentDates);
     }
 
@@ -57,6 +57,8 @@ class CRM_ManualDirectDebit_Form_SetUp extends CRM_Core_Form {
     $this->add('text', 'bank_account_holder', E::ts('Name of Account holder:'), ['size' => 40], TRUE);
     $this->add('text', 'bank_account_number', E::ts('Account number:'), ['size' => 40], TRUE);
     $this->add('text', 'bank_sort_code', E::ts('Sort code:'), ['size' => 40], TRUE);
+    $this->add('hidden', 'contribution_id', $this->contributionId);
+    $this->add('hidden', 'contact_id', $contribution['contact_id']);
 
     $this->addButtons([
       [
@@ -70,6 +72,68 @@ class CRM_ManualDirectDebit_Form_SetUp extends CRM_Core_Form {
 
   public function postProcess() {
     parent::postProcess();
+
+    $values = $this->exportValues();
+
+    $contributionRecur = civicrm_api3('Contribution', 'getsingle', [
+      'id' => $values['contribution_id'],
+      'api.ContributionRecur.get' => [],
+    ])['api.ContributionRecur.get']['values'][0];
+
+    $cycleDay = $this->getCycleDay($values['payment_dates'], $contributionRecur['frequency_unit']);
+
+    $test = civicrm_api3('ContributionRecur', 'create', [
+      'id' => $contributionRecur['id'],
+      'payment_instrument_id' => 'direct_debit',
+      'cycle_day' => $cycleDay,
+    ]);
+
+    $defaultDDCode = civicrm_api3('OptionValue', 'get', [
+      'sequential' => 1,
+      'option_group_id' => 'direct_debit_codes',
+      'label' => "0N",
+    ])['values'][0]['value'];
+
+    $now = new DateTime();
+    $mandateValues = [
+      'entity_id' => $values['contact_id'],
+      'bank_name' => $values['bank_name'],
+      'account_holder_name' => $values['bank_account_holder'],
+      'ac_number' => $values['bank_account_number'],
+      'sort_code' => $values['bank_sort_code'],
+      'dd_code' => $defaultDDCode,
+      'start_date' => $now->format('Y-m-d H:i:s'),
+    ];
+
+    $storageManager = new CRM_ManualDirectDebit_Common_MandateStorageManager();
+    $mandate = $storageManager->saveDirectDebitMandate($values['contact_id'], $mandateValues);
+    $storageManager->assignRecurringContributionMandate($contributionRecur['id'], $mandate->id);
+    $storageManager->assignContributionMandate($values['contribution_id'], $mandate->id);
+
+    $url = CRM_Utils_System::url('civicrm/direct_debit/setup/confirmation');
+    CRM_Utils_System::redirect( $url);
+  }
+
+  /**
+   * @param $paymentDay
+   * @param $frequencyUnit
+   * @return integer
+   */
+  private function getCycleDay($paymentDay, $frequencyUnit) {
+
+    if ($frequencyUnit != 'year') {
+      return $paymentDay;
+    }
+
+    $now = new DateTime();
+    $cycleDate = DateTime::createFromFormat('Y-m-d', $now->format('Y-m'. '-' . $paymentDay));
+
+    //If cycle date is in the past, set cycle date should be next month.
+    if ($cycleDate < $now) {
+      $cycleDate = $cycleDate->modify('next month');
+    }
+
+    return (int) $cycleDate->format('z') + 1;
   }
 
   /**
@@ -92,12 +156,12 @@ class CRM_ManualDirectDebit_Form_SetUp extends CRM_Core_Form {
     $locale = civicrm_api3('Setting', 'get', [
       'sequential' => 1,
       'return' => ["lcMessages"],
-    ])['values']['lcMessages'];
+    ])['values'][0]['lcMessages'];
     $ordinalSuffixFormatter = new NumberFormatter($locale, NumberFormatter::ORDINAL);
 
     $paymentDates = [];
-    foreach ($settings['payment_collection_run_dates'] as $day){
-      $paymentDates[$day] =  $ordinalSuffixFormatter->format($day);
+    foreach ($settings['payment_collection_run_dates'] as $day) {
+      $paymentDates[$day] = $ordinalSuffixFormatter->format($day);
     }
 
     return $paymentDates;
