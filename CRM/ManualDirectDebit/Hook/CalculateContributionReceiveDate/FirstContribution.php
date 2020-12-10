8966202fe0135120c41c19c8f9ee3ca08a08ff5f
Camilo Rodriguez <camilo@compucorp.co.uk>
2020-12-10 13:27:33 +0000

    IO-126: Alter Receive Date of Instalments After First Based on Settings

--- CRM/ManualDirectDebit/Hook/CalculateContributionReceiveDate/FirstContribution.php
+++ CRM/ManualDirectDebit/Hook/CalculateContributionReceiveDate/FirstContribution.php
@@ -7,74 +7,7 @@ use CRM_ManualDirectDebit_Common_SettingsManager as SettingsManager;
  * Implements hook to calculate the receive date of the first contribution of a
  * payment plan.
  */
-class CRM_ManualDirectDebit_Hook_CalculateContributionReceiveDate_FirstContribution {
-
-  /**
-   * Start date of the payment plan and the receive date of first instalment.
-   *
-   * @var string
-   */
-  private $receiveDate = '';
-
-  /**
-   * List of parameters being used to create the first instalment.
-   *
-   * @var array
-   */
-  private $params = [];
-
-  /**
-   * Array with Direct Debit extension settings.
-   *
-   * @var array
-   */
-  private $ddSettings = [];
-
-  /**
-   * The DirectDebit payment instrument data.
-   *
-   * @var array
-   */
-  private $directDebitPaymentInstrument = [];
-
-  /**
-   * CRM_ManualDirectDebit_Hook_CalculateContributionReceiveDate_FirstContribution constructor.
-   *
-   * @param $receiveDate
-   * @param $params
-   * @param \CRM_ManualDirectDebit_Common_SettingsManager $settingsManager
-   *
-   * @throws \CRM_Extension_Exception
-   * @throws \CiviCRM_API3_Exception
-   */
-  public function __construct(&$receiveDate, &$params, SettingsManager $settingsManager) {
-    $this->receiveDate =& $receiveDate;
-    $this->params =& $params;
-    $this->ddSettings = $settingsManager->getManualDirectDebitSettings();
-    $this->directDebitPaymentInstrument = $this->getDDPaymentMethod();
-  }
-
-  /**
-   * Obtains the data for the Direct Debit payment instrument.
-   *
-   * @return mixed
-   * @throws \CRM_Extension_Exception
-   * @throws \CiviCRM_API3_Exception
-   */
-  private function getDDPaymentMethod() {
-    $result = civicrm_api3('OptionValue', 'get', [
-      'sequential' => 1,
-      'name' => 'direct_debit',
-      'option_group_id' => 'payment_instrument',
-      'options' => ['limit' => 0],
-    ]);
-
-    if ($result['count'] > 0) {
-      return $result['values'][0];
-    }
-
-    throw new CRM_Extension_Exception('Could not obtain DD Payment Instrument!');
-  }
+class CRM_ManualDirectDebit_Hook_CalculateContributionReceiveDate_FirstContribution extends CRM_ManualDirectDebit_Hook_CalculateContributionReceiveDate_ContributionBase {
 
   /**
    * Calculates receive date for payment plan if payment method is DD.
@@ -96,23 +29,6 @@ class CRM_ManualDirectDebit_Hook_CalculateContributionReceiveDate_FirstContribut
 
     $nextPaymentCollectionDate = $this->getNextValidDateAfter($nextInstructionRunDate, $this->ddSettings['payment_collection_run_dates']);
     $this->receiveDate = $nextPaymentCollectionDate->format('Y-m-d H:i:s');
-  }
-
-  /**
-   * Checks if the contribution is being paid for with direct debit.
-   *
-   * @return bool
-   */
-  private function isDirectDebit() {
-    if ($this->params['payment_instrument_id'] === 'direct_debit') {
-      return TRUE;
-    }
-
-    if ($this->params['payment_instrument_id'] === $this->directDebitPaymentInstrument['value']) {
-      return TRUE;
-    }
-
-    return FALSE;
   }
 
   /**
