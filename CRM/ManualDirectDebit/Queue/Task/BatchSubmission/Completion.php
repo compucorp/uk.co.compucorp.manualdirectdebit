804bf09edeaa846403b22e66e10baba95bb7928d
Ahed <ahed@compucorp.co.uk>
2021-01-01 19:56:32 +0200

    MAE-360: Count batch items on submission

--- CRM/ManualDirectDebit/Queue/Task/BatchSubmission/Completion.php
+++ CRM/ManualDirectDebit/Queue/Task/BatchSubmission/Completion.php
@@ -6,11 +6,19 @@ class CRM_ManualDirectDebit_Queue_Task_BatchSubmission_Completion {
     $session = CRM_Core_Session::singleton();
     $loggedInUserId = $session->get('userID');
 
+    $sqlQuery = 'SELECT COUNT(*) AS item_count
+               FROM civicrm_entity_batch
+               WHERE civicrm_entity_batch.batch_id = %1;';
+    $itemCount = CRM_Core_DAO::singleValueQuery($sqlQuery, [
+      1 => [$batchId, 'Integer'],
+    ]);
+
     civicrm_api3('Batch', 'create', [
       'id' => $batchId,
       'status_id' => 'Submitted',
       'modified_date' => date('YmdHis'),
-      'modified_id' => $loggedInUserId
+      'modified_id' => $loggedInUserId,
+      'item_count' => $itemCount,
     ]);
 
     $ctx->log->info('Changing batch with Id : ' . $batchId . ' to Submitted');
